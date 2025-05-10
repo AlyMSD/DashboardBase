5eb90c8658914b28b9b997050960cf878e527fbb
AlyMSD <55713679+AlyMSD@users.noreply.github.com>
2025-05-10 11:26:28 -0500

    Update sliceapp.js

--- sliceapp.js
+++ sliceapp.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard    from "./Dashboard";
-import MarketDetail from "./MarketDetail";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import MarketDetail from './MarketDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/market/:name" element={<MarketDetail />} />
+        <Route path="/market/:id/:nf/:name" element={<MarketDetail />} />
       </Routes>
     </BrowserRouter>
   );
